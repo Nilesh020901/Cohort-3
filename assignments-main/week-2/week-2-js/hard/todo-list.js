d768c01ed1ce7faa464865992182d23b7576840d
Nilesh020901 <nileshsoni0209@gmail.com>
2024-11-13 19:03:43 +0530

    todos done

--- assignments-main/week-2/week-2-js/hard/todo-list.js
+++ assignments-main/week-2/week-2-js/hard/todo-list.js
@@ -11,6 +11,55 @@
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
+
+  //Add todo
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  //remove todo
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      this.todos.splice(indexOfTodo, 1);
+    }
+    else {
+      console.log('Todo not found');
+    }
+  }
+
+  //update todos
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.todos.length) {
+      this.todos[index] = updatedTodo;
+    }
+    else {
+      console.log('Todo not found');
+    }
+  }
+
+  //all todos
+  getAll() {
+    return this.todos;
+  }
+
+  //get index of todos
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.todos.length) {
+      return this.todos[indexOfTodo];
+    }
+    else {
+      console.log('Todo not found');
+      return null;
+    }
+  }
+
+  //clear todos
+  clear() {
+    return this.todos = [];
+  }
 
 }
 

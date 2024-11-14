f6e72bea9d893e31d04bbb0bcbdb58e3fd8c3f7a
Nilesh020901 <nileshsoni0209@gmail.com>
2024-11-14 18:33:53 +0530

    promise-chain done

--- assignments-main/week-2/week-2-async-js/hard (promises)/4-promise-chain.js
+++ assignments-main/week-2/week-2-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,47 @@
  */
 
 function wait1(t) {
-
-}
-
-function wait2(t) {
-
-}
-
-function wait3(t) {
-
-}
-
-function calculateTime(t1, t2, t3) {
-
-}
+    return new Promise((resolve)=>{
+      setTimeout(()=>{
+        resolve(`First promise resolved after ${t} seconds`);
+      }, t*1000);
+    });
+  }
+  
+  function wait2(t) {
+    return new Promise((resolve)=>{
+      setTimeout(()=>{
+        resolve(`Second promise resolved after ${t} seconds`);
+      }, t*1000);
+    });
+  }
+  
+  function wait3(t) {
+    return new Promise((resolve)=>{
+      setTimeout(()=>{
+        resolve(`Third promise resolved after ${t} seconds`);
+      }, t*1000);
+    });
+  }
+  
+  function calculateTime(t1, t2, t3) {
+    const st = Date.now(); //starttime
+  
+    //chain promise call
+    return wait1(t1) //1st fun call 
+    .then(()=>wait2(t2)) //after done then 2nd fun call
+    .then(()=>wait3(t3)) //after done then 3rd fun call
+    .then(()=>{
+      const et = Date.now(); //endtime
+      const total = et - st;
+      const totalSec = total / 1000;
+      console.log('All promises resolved sequentially');
+      console.log('Total time taken: ' + totalSec + 's');
+      return total;
+    })
+    .catch((error)=>{
+      console.error('One of the promises rejected:', error);
+    })
+  }
 
 module.exports = calculateTime;

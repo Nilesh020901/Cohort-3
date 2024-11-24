083ab87123929bfe8dffcb9f991206246fc87046
Nilesh020910 <nileshsoni0209@gmail.com>
2024-11-24 11:14:15 +0530

    push

--- assignments-main/week-4/middlewares/01-ratelimitter.js
+++ assignments-main/week-4/middlewares/01-ratelimitter.js
@@ -16,6 +16,24 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) => {
+  const userId = req.headers['user-id'];
+
+  if (!userId) {
+    return res.status(400).json({error: 'User ID is required in headers'});
+  }
+
+  if (!numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] = 0;
+  }
+
+  if (numberOfRequestsForUser[userId] > 5) {
+    return res.status(404).json({error: 'User Id hit more 5 times'})
+  }
+
+  next();
+});
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });

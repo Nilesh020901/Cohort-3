baac7815b25d00c84d04738154002866ffd9071c
Nilesh020910 <nileshsoni0209@gmail.com>
2024-11-25 10:06:49 +0530

    push

--- assignments-main/week-6/Class_assignment/index.js
+++ assignments-main/week-6/Class_assignment/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+const users = [];
+
 
 app.post("/signup", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    users.push({
+        username: username,
+        password: password
+    }) 
+
+    res.json({
+        msg: "You are signed in"
+    })
 });
 
 app.post("/signin", function(req, res) {

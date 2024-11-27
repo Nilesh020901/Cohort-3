bc5f07cfc742ed1fb1cac3d67760777124a8dc9f
Nilesh020910 <nileshsoni0209@gmail.com>
2024-11-27 23:03:01 +0530

    push

--- assignments-main/week-6/Class_assignment/index.js
+++ assignments-main/week-6/Class_assignment/index.js
@@ -4,6 +4,18 @@ app.use(express.json());
 
 const users = [];
 
+// return random long string
+function generateToken() {
+    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        // use a simple function here
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
 
 app.post("/signup", function(req, res) {
     const username = req.body.username;
@@ -20,7 +32,29 @@ app.post("/signup", function(req, res) {
 });
 
 app.post("/signin", function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
 
+    const user = users.find(user => user.username === username && user.password === password);
+    
+    if (user) {
+        const token  = generateToken();
+        user.token = token;
+        res.send({
+            token
+        })
+        console.log(users);
+    }
+    else {
+        res.json(403).send({
+            message:"Invalid username or password"
+        })
+    }
+});
+
+app.get("/me", function(req, res) {
+    const token = req.headers.token
+    
 });
 
 app.listen(3000);

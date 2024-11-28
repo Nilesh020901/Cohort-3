cbb02a1fc41fa9db8e6b8a329dd601e8bc7b9d69
Nilesh020901 <nileshsoni0209@gmail.com>
2024-11-28 19:07:59 +0530

    push

---
+++ assignments-main/week-6/Assignment_Class/jwtdecode.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+function decodeJwt(token) {
+	const decoded jwt.decode(token);
+	false
+	if (decoded) {
+
+		return true
+	}
+	return false
+}
+console.log(decodeJwt('9afza5vpuemopj82ffu65gt9sh9v8n9s'));

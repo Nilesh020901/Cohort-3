fa7bc8ec0b1627e60b7bc50d9f3a6fab34d85a56
Nilesh020901 <nileshsoni0209@gmail.com>
2024-12-16 14:21:55 +0530

    push

--- assignments-main/week-9/timerApp/src/components/Timer.jsx
+++ assignments-main/week-9/timerApp/src/components/Timer.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Timer = () => {
   return (
-    <div>Timer</div>
+    <div className= {style.timerApp}>
+      <div className= {style.timerDisplay}>
+        <div className= {style.timerCircle}>
+          <div className= {style.timerTime}>
+            {editState.feild === 'hours' ? (
+              <input className= {style.timeInput} type='text' value={editState.value} onChange={handleInputChange} onBlur={() => handleEditFeild('hours')} autoFocus />
+            ):(
+              <span className={style.timeUnit} onClick={()=> handleEditFeild('hours')}>{hours}</span>
+            )}
+            :
+            {editState.feild === 'minutes' ? (
+              <input className= {style.timerInput} type='text' value={editState.value} onChange={handleInputChange} onBlur={() => handleEditFeild('minutes')} autoFocus />
+            ):(
+              <span className= {style.timeUnit} onClick={()=> handleEditFeild('hours')}>{minutes}</span>
+            )}
+            :
+            {editState.feild === 'seconds' ? (
+              <input className={style.timeInput} type='text' value={editState.value} onChange={handleInputChange} onBlur={() => handleEditFeild('seconds')} autoFocus />
+            ):(
+              <span className={style.timeUnit} onClick={()=> handleEditFeild('seconds')}>{seconds}</span>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className={style.actionButtons}>
+        <button className={style.actionButton} onClick={() => setIsRunning(!isRunning)}>{isRunning ? }</button>
+      </div>
+    </div>
   )
 }
 

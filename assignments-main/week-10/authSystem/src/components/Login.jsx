17825a87f08dae009defd06041da9526f44e4f8c
Nilesh020910 <nileshsoni0209@gmail.com>
2024-12-17 22:20:02 +0530

    push

--- assignments-main/week-10/authSystem/src/components/Login.jsx
+++ assignments-main/week-10/authSystem/src/components/Login.jsx
@@ -18,7 +18,7 @@ const Login = ({ onLogin: propOnLogin }) => {
     <div style={{ display: "flex", flexDirection: "column", gap: "1rem", maxwidth: "300px", margin: "0 auto" }}>
       <div style={{ display: "flex", flexDirection: "column", gap: "0.5rem"}}>
         <label htmlFor="username" style={{ fontweight: "bold" }}>Username</label>
-        <input id="username" value={Username} onChange={(e)=>setUsername(e.taget.value)} placeholder="Enter your username" style={{ padding: "0.5rem", borderRadius: "4px", border: "1px solid #ccc" }} />
+        <input id="username" value={username} onChange={(e)=>setUsername(e.target.value)} placeholder="Enter your username" style={{ padding: "0.5rem", borderRadius: "4px", border: "1px solid #ccc" }} />
       </div>
       <button onClick={handleLogin} style={{ backgroundColor: "#3f98b5", color: "white", border: "none", padding: "0.5rem 1rem", borderRadius: "4px", cursor: "pointer"}}>Login</button>
     </div>

2b5992bd1bb37e60f700d1f0b51a692f09b9da3e
Nilesh020910 <nileshsoni0209@gmail.com>
2024-12-19 00:00:08 +0530

    push

---
+++ assignments-main/week-10/fetchapi/src/App.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import './App.css'
+import FetchApiExample from './components/FetchApi.jsx'
+
+export default function App() {
+  return (
+    <main>
+      <>
+        <FetchApiExample />
+      </>
+    </main>
+  )
+}

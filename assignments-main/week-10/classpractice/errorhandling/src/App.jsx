ffe5154a71af7b8ed2860fdf47471a940b861ce3
Nilesh020910 <nileshsoni0209@gmail.com>
2024-12-20 23:30:05 +0530

    push

---
+++ assignments-main/week-10/classpractice/errorhandling/src/App.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+import './App.css'
+import ErrorHandling from './components/ErrorHandling.jsx'
+
+export default function App() {
+  return (
+    <main>
+      <>
+        <ErrorHandling />
+      </>
+   </main>
+  )
+}

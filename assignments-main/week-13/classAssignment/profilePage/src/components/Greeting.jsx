3659478b06195e6f9a9fb62a99644a14fe9e936b
Nilesh020901 <nileshsoni0209@gmail.com>
2024-12-28 17:31:58 +0530

    push

---
+++ assignments-main/week-13/classAssignment/profilePage/src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Greeting = () => {
+    return (
+        <div className="mb-4">
+            <h1 className="text-2xl font-bold">Good morning, Prabhleen! 👋</h1>
+            <p className="text-gray-600">Monday, 14 October 2024</p>
+        </div>
+    );
+};
+
+export default Greeting;

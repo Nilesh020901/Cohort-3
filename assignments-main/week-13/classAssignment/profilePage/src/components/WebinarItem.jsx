265519a8e72f99910166ec0215e2c34750b214bc
Nilesh020910 <nileshsoni0209@gmail.com>
2024-12-30 00:54:25 +0530

    push

--- assignments-main/week-13/classAssignment/profilePage/src/components/WebinarItem.jsx
+++ assignments-main/week-13/classAssignment/profilePage/src/components/WebinarItem.jsx
@@ -12,7 +12,7 @@ const WebinarItem = ({ time, status, title }) => {
                 <div>
                     <div className="flex items-center gap-2">
                         <div>{status}</div>
-                        <div><svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <div className={status === 'Live' ? 'text-red-400' : 'text-blue-400'}><svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
                             <path d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5ZM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06Z" />
                         </svg>
                         </div>

807759aa7bcf55cee5b9a5f5bf0b498ddd3cc7b8
Nilesh020910 <nileshsoni0209@gmail.com>
2024-12-29 10:24:33 +0530

    push

--- assignments-main/week-13/classAssignment/profilePage/src/components/Header.jsx
+++ assignments-main/week-13/classAssignment/profilePage/src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div>
-            <img src="#" alt="header-img" />
+        <div >
+            <img src="https://img.freepik.com/free-vector/stylish-glowing-digital-red-lines-banner_1017-23964.jpg?t=st=1735445740~exp=1735449340~hmac=d33c2176171e46917fc372a8551a420c2178f8b4881d4d308e5285d213145ba6&w=1380" alt="header-img"
+            className="w-screen h-28" />
         </div>
-    )
-}
+    );
+};
+
+export default Header;
--- assignments-main/week-13/classAssignment/profilePage/src/components/Sidebar.jsx
+++ assignments-main/week-13/classAssignment/profilePage/src/components/Sidebar.jsx
@@ -9,9 +9,16 @@ const Sidebar = () => {
             </div>
             <nav>
                 <ul className="space-y-4">
-                    <li className="p-2 hover:bg-blue-700"><span>Home</span><span>icon</span></li>
-                    <li className="p-2 hover:bg-blue-700"><span>Home</span><span>icon</span></li>
-                    <li className="p-2 hover:bg-blue-700"><span>Home</span><span>icon</span></li>
+                    <li className="p-2 hover:bg-blue-700"><span>Home</span><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+  <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+</svg>
+</span></li>
+                    <li className="p-2 hover:bg-blue-700"><span>Home</span><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+</svg>
+</span></li>
+                    <li className="p-2 hover:bg-blue-700"><span>Home</span><span><FontAwesomeIcon icon="fa-regular fa-calendar" /></span></li>
                     <li className="p-2 hover:bg-blue-700"><span>Home</span><span>icon</span></li>
                     <li className="p-2 hover:bg-blue-700"><span>Home</span><span>icon</span></li>
                 </ul>

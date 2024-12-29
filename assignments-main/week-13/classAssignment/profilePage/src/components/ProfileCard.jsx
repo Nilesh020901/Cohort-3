f9131987e9d365a22d17e648cc865e12119f9b65
Nilesh020910 <nileshsoni0209@gmail.com>
2024-12-30 00:54:25 +0530

    push

--- assignments-main/week-13/classAssignment/profilePage/src/components/ProfileCard.jsx
+++ assignments-main/week-13/classAssignment/profilePage/src/components/ProfileCard.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import nileshImg1 from "./images/nilesh-img1.jpg"
 
 const ProfileCard = () => {
     return (
-        <div className="flex flex-col items-center justify-center rounded-lg shadow-lg h-80 -translate-y-12 bg-white">
+        <div className="flex flex-col items-center justify-center rounded-lg shadow-lg h-96 -translate-y-12 bg-white">
             <img
-                src="https://dummyimage.com/80x80/cccccc/ffffff"
+                src={nileshImg1}
                 alt="Profile"
                 className="w-24 h-24 rounded-2xl"
             />
             <div className='mt-6 text-center'>
-                <h2 className="font-bold text-lg mb-2">Nilesh Soni</h2>
+                <h2 className="font-bold text-lg mb-2 text-sky-950">Nilesh Soni</h2>
                 <p className="text-gray-600 mb-1 font-medium">nileshsoni@gmail.com</p>
                 <p className="text-gray-600 mb-2 font-medium">8356004765</p>
                 <p className="text-gray-600 font-medium">Mumbai, India</p>

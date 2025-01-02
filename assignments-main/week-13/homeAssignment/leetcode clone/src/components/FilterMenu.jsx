8c75a1d58a05cc5f77d1161c2186de0a7dd03d58
Nilesh020901 <nileshsoni0209@gmail.com>
2025-01-02 14:59:47 +0530

    push

---
+++ assignments-main/week-13/homeAssignment/leetcode clone/src/components/FilterMenu.jsx
@@ -0,0 +1,12 @@
+const FilterMenu = () => {
+    return (
+        <div className="bg-gray-700 text-white p-4 rounded-md">
+            <h3 className="text-lg font-bold">Filters</h3>
+            <div className="">
+                <label></label>
+            </div>
+        </div>
+    )
+}
+
+export default FilterMenu;

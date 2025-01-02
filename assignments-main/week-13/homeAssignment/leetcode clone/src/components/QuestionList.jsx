059fb182f644ef844eec55c50db4cfa1f19bf55e
Nilesh020901 <nileshsoni0209@gmail.com>
2025-01-02 16:47:14 +0530

    push

--- assignments-main/week-13/homeAssignment/leetcode clone/src/components/QuestionList.jsx
+++ assignments-main/week-13/homeAssignment/leetcode clone/src/components/QuestionList.jsx
@@ -1,5 +1,16 @@
-const QuestionList = () => {
-
-}
+const QuestionList = ({ questions }) => {
+    return (
+        <div className="mt-4">
+            {questions.map((q) => (
+                <div
+                key={q.id}
+                className="flex justify-between items-center p-4 bg-gray-800 text-white mb-2 rounded-md">
+                    <p>{q.title}</p>
+                    <span className="text-green-400">{ q.difficulty }</span>
+                </div>
+            ))}
+        </div>
+    );
+};
 
 export default QuestionList;

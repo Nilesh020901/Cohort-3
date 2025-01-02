5e30237bbe7ff39e735208281bc79693af87e292
Nilesh020901 <nileshsoni0209@gmail.com>
2025-01-02 13:20:31 +0000

    push

--- assignments-main/week-13/homeAssignment/leetcode clone/src/pages/LandingPages.jsx
+++ assignments-main/week-13/homeAssignment/leetcode clone/src/pages/LandingPages.jsx
@@ -11,7 +11,7 @@ const LandingPage = () => {
     const questions = useRecoilValue(questionState);
 
     return(
-        <div className="flex h-screen">
+        <div className="flex h-screen bg-zinc-900">
             <Sidebar />
             <div className="flex-1 bg-gray-900 p-4">
                 <Header />

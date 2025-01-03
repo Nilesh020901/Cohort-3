0a826746021ed0049a901a621bad1c0ff541f47d
Nilesh020910 <nileshsoni0209@gmail.com>
2025-01-03 09:52:18 +0530

    push

---
+++ assignments-main/week-15/brainly/src/db.ts
@@ -0,0 +1,69 @@
+import mongoose, { Schema, Document, Types } from "mongoose";
+
+mongoose.connect("Your Connection String");
+
+interface IUser extends Document {
+    username: string;
+    password: string;
+}
+
+const userSchema: Schema = new mongoose.Schema({
+    username: { type: String, unique: true },
+    password: { type: String},
+});
+const User = mongoose.model<IUser>("User", userSchema);
+
+interface ITag extends Document {
+    title: string;
+}
+
+const tagSchema: Schema = new mongoose.Schema({
+    title: { type: String, required: true, unique: true },
+})
+const Tag = mongoose.model<ITag>("Tag", tagSchema);
+
+const contentTypes = [
+    "image", 
+    "video", 
+    "article", 
+    "audio", 
+    "document", 
+    "presentation", 
+    "spreadsheet", 
+    "code-snippet", 
+    "ebook", 
+    "podcast", 
+    "webpage"
+  ]; // Extended content types  
+
+interface IContent extends Document {
+    link: String;
+    type: string;
+    title: string;
+    tags: Types.ObjectId[];
+    userId: Types.ObjectId;
+}
+
+const contentSchema: Schema = new mongoose.Schema({
+    link: { type: String, required: true},
+    type: { type: String, enum: contentTypes, required: true },
+    title: { type: String, required: true},
+    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: "Tag"}],
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+const Content = mongoose.model<IContent>("Content", contentSchema);
+
+interface ILink extends Document {
+    hash: string;
+    userId: Types.ObjectId;
+}
+
+const linkSchema: Schema = new mongoose.Schema({
+    hash: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+const Link = mongoose.model<ILink>("Link", linkSchema);
+
+export {User, Tag, Content, Link};

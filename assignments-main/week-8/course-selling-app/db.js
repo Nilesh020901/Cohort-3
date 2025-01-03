053abfe0f3a250ea7077932373fd29dd3224d76f
Nilesh020901 <nileshsoni0209@gmail.com>
2025-01-03 11:02:27 +0000

    push

---
+++ assignments-main/week-8/course-selling-app/db.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+
+const userSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const adminSchema = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String,
+});
+
+const courseSchema = new Schema({
+    title: String,
+    description: String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+})

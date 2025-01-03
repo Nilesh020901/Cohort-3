82c773a0edcf3e02a2a8b7dd3e8c8c4edf49ac82
Nilesh020910 <nileshsoni0209@gmail.com>
2025-01-03 09:52:18 +0530

    push

--- assignments-main/week-15/brainly/src/index.ts
+++ assignments-main/week-15/brainly/src/index.ts
@@ -1,11 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import bcript from "bcrypt";
+import { z } from "zod";
+import { User } from "./db";
 
-const app = express()
+const app = express();
+app.use(express.json());
+
+// Define Zod schema for validation
+const signupSchema = z.object({
+    username: z.string()
+    .min(3, { message: "Username should be at least 3 characters." })
+    .max(10, { message:"Username should not exceed 10 characters." }),
+    password: z.string()
+    .min(8, { message: "Password must be at least 8 characters." })
+    .max(16, { message: "Password must not exceed 16 characters."})
+    .regex(/())
+})
 
 app.post("/api/v1/signup", (req, res) => {
-
 })
 
 app.post("/api/v1/signin", (req, res) => {

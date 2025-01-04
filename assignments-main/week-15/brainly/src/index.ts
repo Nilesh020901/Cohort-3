4c6543b041663c6a5b1648812dd886db9d54d7a9
Nilesh020910 <nileshsoni0209@gmail.com>
2025-01-04 09:38:55 +0530

    push

--- assignments-main/week-15/brainly/src/index.ts
+++ assignments-main/week-15/brainly/src/index.ts
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { z } from "zod";
-import { User } from "./db";
+import { Content, User } from "./db";
+//import middleware
 
 const app = express();
 app.use(express.json());
@@ -55,20 +56,118 @@ app.post("/api/v1/signup", async (req: Request, res: Response): Promise<any> => 
     }
 });
 
-app.post("/api/v1/signin", (req, res) => {
+app.post("/api/v1/signin", async (req: Request, res: Response): Promise<any> => {
+    try {
+        const { username, password } = signupSchema.parse(req.body);
 
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(403).json({ message: "Wrong username or password."});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid) {
+            return res.status(403).json({ message: "Wrong password" });
+        }
+
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET,{ expiresIn: "1h" });
+        return res.status(200).json({ message:"Signin Successfully", token });
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({ errors: error.errors });
+        }
+        console.error("Error during sign-in", error);
+        return res.status(500).json({ message:"Internal server error."})
+    }
 })
 
-app.post("/api/v1/content", (req, res) => {
-
+const contentValidationSchema = z.object({
+    type: z.enum([
+        "image",
+        "video",
+        "article", 
+        "audio", 
+        "document", 
+        "presentation", 
+        "spreadsheet", 
+        "code-snippet", 
+        "ebook", 
+        "podcast", 
+        "webpage",
+    ]),
+    link: z.string().url({message: "Invalid URL format."}),
+    title: z.string().min(1, { message: "Title is required." }),
+    tags: z.array(z.string()).optional(),
 })
 
-app.get("/api/v1/content", (req, res) => {
+app.post("/api/v1/content", userMiddleware, async (req: Request, res: Response): Promise<any> => {
+    try {
+        //check valid hai ya nahi
+        const { type, link, title, tags } = contentValidationSchema.parse(req.body);
+        
+        //new content banao
+        const newContent = await content.create({
+            type,
+            link,
+            title,
+            userId: req.userId, //userId is add by middleware
+            tags: tags || [], //tags agar empty hai toh []
+        });
 
-})
+        res.status(201).json({ message: "Content added successfully.", content: newContent });
+    } catch (error) {
+        if (error instanceof z.ZodError) {
+            return res.status(400).json({ errors: error.errors });
+        }
 
-app.post("/api/v1/brain/share", (req, res) => {
+        console.error("Error adding content:", error);
+        return res.status(500).json({ message: "Internal server error." });
+    }
+});
 
+app.get("/api/v1/content", userMiddleware, async (req: Request, res: Response): Promise<any> => {
+    try {
+        //fetch all content
+        const content = await Content.find({ userId: req.userID })
+        .populate("tags", "name") //populate tags details
+        .select("_id type link title tags");
+
+        //Transform content response ke liye
+        const responseContent = content.map((item) => ({
+            id: item._id,
+            type: item.type,
+            link: item.link,
+            title: item.title,
+            tags: item.tags,
+        }));
+
+        res.status(200).json({ content: responseContent });
+    } catch (error) {
+        console.error("Error fetching content:", error);
+        return res.status(500).json({ message: "Internal server error." });
+    }
+});
+
+app.post("/api/v1/brain/share", userMiddleware, (req: Request, res: Response): Promise<any> => {
+    try {
+        const { contentId } = req.body;
+
+        //validate
+        if(!contentId) {
+            return res.status(404).json({ message: "Content not found." })
+        }
+
+        //check content user ka hai
+        if (content.userId.toString() != req.userId) {
+            return res.status(403).json({ message: "You do not have permission to delete this content." });
+        }
+
+        await Content.findByIdAndDelete(contentId);
+        res.status(200).json({ message: "Content Deleted Succesfully"})
+    } catch (error) {
+        console.error("Error delete content: ", error);
+        return res.status(500).json({ message: "Internal server error." })
+    }
 })
 
 app.post("/api/v1/brain/:shareLink", (req, res) => {

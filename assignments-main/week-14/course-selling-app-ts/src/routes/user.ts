411ac67eb5d8f3fc14ac9ebe4698bc7ade933ad3
Nilesh020910 <nileshsoni0209@gmail.com>
2025-01-07 08:12:54 +0530

    push

---
+++ assignments-main/week-14/course-selling-app-ts/src/routes/user.ts
@@ -0,0 +1,64 @@
+import { Request, Response, Router } from "express";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+import JWT_USER_PASSWORD from "../config";
+import { userMiddleware } from "../middleware/user";
+import jwt from "jsonwebtoken";
+import { userModel, courseModel, purchaseModel } from "../db";
+const userRouter = Router();
+import { courseRouter } from "./course"
+
+const signupSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6, { message: "atleast 6 letters" }),
+    firstName: z.string().min(1, { message: "atleat 1 letters" }),
+    lastName: z.string().min(1, { message: "atleast 1 letters" }),
+});
+
+const signinSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6, { message: "atleast 6 letters" }),
+});
+
+userRouter.post("/signup", async (req:Request, res:Response) => {
+    try {
+        const { email, password, firstName, lastName } = signupSchema.parse(req.body);
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await userModel.create({
+            email,
+            password: hashedPassword,
+            firstName,
+            lastName
+        });
+        res.status(200).json({
+            message: "Successfully Signup",
+        })
+    } catch (error:any) {
+        res.status(500).json({
+            message: "Signup Failed",
+        })
+    }
+})
+
+userRouter.post("/signin", async (req:Request, res:Response): Promise<any> => {
+    try {
+        const { email, password } = signinSchema.parse(req.body)
+        const user = await userModel.findOne({ email });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({
+                message: "Incorrect Value",
+            })
+        }
+
+        const token = jwt.sign({ id: user._id}, JWT_USER_PASSWORD, { expiresIn: "1h" });
+        res.cookie("auth_token", token, { httpOnly: true }),
+        res.status(200).json({
+            message: "Succesfully Signin", token
+        })
+    } catch (error:any) {
+        res.status(500).json({
+            message: error.message || "Succesfully Signin"
+        })
+    }
+})

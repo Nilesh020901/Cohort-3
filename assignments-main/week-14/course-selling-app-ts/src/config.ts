be7da7e7a78153e5a08d979dd036e82f5f071fba
Nilesh020910 <nileshsoni0209@gmail.com>
2025-01-08 10:13:10 +0530

    push

---
+++ assignments-main/week-14/course-selling-app-ts/src/config.ts
@@ -0,0 +1,7 @@
+const JWT_USER_PASSWORD: string = process.env.JWT_USER_PASSWORD as string;
+const JWT_ADMIN_PASSWORD: string = process.env.JWT_ADMIN_PASSWORD as string;
+
+export {
+    JWT_ADMIN_PASSWORD,
+    JWT_USER_PASSWORD
+}

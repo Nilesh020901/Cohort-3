c71576d2d1e9c52b9499bb9e3f55b1c90825d97e
Nilesh020910 <nileshsoni0209@gmail.com>
2025-01-08 10:13:10 +0530

    push

--- assignments-main/week-14/course-selling-app-ts/src/index.ts
+++ assignments-main/week-14/course-selling-app-ts/src/index.ts
@@ -1,5 +1,34 @@
 import dotenv from "dotenv";
-import express, { Application } from "express";
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 
 import { userRouter } from "./routes/user";
+import { adminRouter } from "./routes/admin";
+import { courseRouter } from "./routes/course";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/course", courseRouter);
+
+async function main(): Promise<void> {
+    try {
+        const MONGO_URL: string = process.env.MONGO_URL as string;
+        await mongoose.connect(MONGO_URL);
+        console.log('Connected to MongoDB');
+
+        app.listen(3000, () => {
+            console.log("Listening on port 3000");
+        });
+    } catch (error:any) {
+        console.error("Error connecting to MongoDB:", error);
+        process.exit(1);
+    }
+}
+
+main();

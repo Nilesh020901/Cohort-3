3c08fd7406d32703a50ff50301ae57c04ec4eca6
Nilesh020910 <nileshsoni0209@gmail.com>
2025-01-19 23:22:39 +0530

    push

---
+++ assignments-main/week-15/brainly/Frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'white': {
+          100: '#FAF8FF',
+          200: '#F9FBFC',
+        },
+        'purple': {
+          300: '#463EBC',
+          400: '#463AC5',
+          500: '#E0E7FF',
+          600: '#5046E4',
+          700: '#4F44C6',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+

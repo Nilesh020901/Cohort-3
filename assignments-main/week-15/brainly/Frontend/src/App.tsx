f81481a63ffe4abe27cda2b4bbd1b5df58ceafa0
Nilesh020910 <nileshsoni0209@gmail.com>
2025-01-20 23:57:21 +0530

    push

--- assignments-main/week-15/brainly/Frontend/src/App.tsx
+++ assignments-main/week-15/brainly/Frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import { Button } from './component/Button'
+import { PlusIcon } from './icons/Plus'
+import { ShareIcon } from './icons/Share'
 
 function App() {
  
 
   return (
-    <>
-     <Button variant={'primary'} text="Add content" />
-    </>
+    <div className='flex justify-end items-center gap-4'>
+      <Button variant={'primary'} text="Add Content" startIcon={<PlusIcon />}></Button>
+      <Button variant={'secondary'} text="Share Brain" startIcon={<ShareIcon />}></Button>
+    </div>
   )
 }
 

7cdb125e1d2ed7f636215e2396808146a11868f1
Nilesh020910 <nileshsoni0209@gmail.com>
2025-02-01 23:16:49 +0530

    push

--- assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/backend/routes/user.js
+++ assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/backend/routes/user.js
@@ -96,6 +96,31 @@ router.put("/", authMiddlewale, async (req, res) => {
 	} catch (error) {
 		res.status(500).json({ message: "Internal Server Error" });
 	}
+});
+
+router.get("/bulk", async (req, res) => {
+	const filter = req.query.filter || "";
+
+	const users = await User.find({
+		$or: [{
+			firstName: {
+				"$regex": filter
+			}
+		}, {
+			lastName: {
+				"$regex": filter
+			}
+		}]
+	});
+
+	res.json({
+		user: users.map(user => ({
+			username: user.username,
+			firstName: user.firstName,
+			lastName: user.lastName,
+			_id: user._id
+		}))
+	})
 })
 
 module.exports = router;

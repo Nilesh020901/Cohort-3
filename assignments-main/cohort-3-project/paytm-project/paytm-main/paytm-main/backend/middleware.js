129505676c1bde5255468b8926544b2e56fd421a
Nilesh020910 <nileshsoni0209@gmail.com>
2025-02-01 09:38:53 +0530

    push

---
+++ assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/backend/middleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("./config");
+
+const authMiddlewar = ((req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        res.status(401).json({ message: "You are not login" });
+    }
+
+    const token = authHeader.split('')[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        if (decoded) {
+            req.userId = decoded.userId;
+            next();
+        }
+        else {
+            res.status(403).json({ message: "Invalid username or password" });
+        }
+    } catch (error) {
+        res.status(503).json({ message: "Internal Server Error" });
+    }
+});
+
+module.exports = {
+    authMiddlewar
+} 

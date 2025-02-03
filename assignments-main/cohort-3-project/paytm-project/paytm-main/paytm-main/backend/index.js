de23029863d09ac232d09ff37a2b1761ee951da5
Nilesh020901 <nileshsoni0209@gmail.com>
2025-02-03 14:19:26 +0530

    push

--- assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/backend/index.js
+++ assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/backend/index.js
@@ -5,9 +5,16 @@ app.use(cors());
 const app = express();
 app.use(express.json());
 const rootRouter = require("./routes/index");
+const mongoose = require("mongoose");
 
 app.use("/api/v1", rootRouter);
 
 
-app.listen(3000);
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
+
+main()
 

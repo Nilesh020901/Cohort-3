afeade322bb8836a6f5b070487ab041b180ee35a
Nilesh020910 <nileshsoni0209@gmail.com>
2025-02-07 15:03:10 +0530

    push

--- assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/frontend/src/pages/Signup.jsx
+++ assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/frontend/src/pages/Signup.jsx
@@ -7,46 +7,40 @@ import { SubHeading } from "../components/SubHeading";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-export const Signup = () => {
+export const Signup = () => {  
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
-    return
-    <div className="bg-slate-300 h-screen flex justify-center">
-        <div className="flex flex-col justify-center">
-            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-                <Heading label={"Sign up"} />
-                <SubHeading label={"Enter your infromation to create an account"} />
-                <InputBox onChange={(e) => {
-                    setFirstName(e.target.value);
-                }} placeholder="Nilesh" label={"First Name"} />
-                <InputBox onChange={(e) => {
-                    setLastName(e.target.value);
-                }} placeholder="Soni" label={"Last Name"} />
-                <InputBox onChange={(e) => {
-                    setUsername(e.target.value);
-                }} placeholder="nileshsoni0209@gmail.com" label={"Email"} />
-                <InputBox onChange={(e) => {
-                    setPassword(e.target.value);
-                }} placeholder="Nilesh@123" label={"Password"} />
-                <div>
-                    <Button onClick={async () => {
-                        const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
-                            username,
-                            firstName,
-                            lastName,
-                            password
-                        });
-                        localStorage.setItem("token", response.data.token)
-                        navigate("/dasboard");
-                    }} label={"Sign up"} />
+    return (  
+        <div className="bg-slate-300 h-screen flex justify-center">
+            <div className="flex flex-col justify-center">
+                <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                    <Heading label={"Sign up"} />
+                    <SubHeading label={"Enter your information to create an account"} />
+                    <InputBox onChange={(e) => setFirstName(e.target.value)} placeholder="Nilesh" label={"First Name"} />
+                    <InputBox onChange={(e) => setLastName(e.target.value)} placeholder="Soni" label={"Last Name"} />
+                    <InputBox onChange={(e) => setUsername(e.target.value)} placeholder="nileshsoni0209@gmail.com" label={"Email"} />
+                    <InputBox onChange={(e) => setPassword(e.target.value)} placeholder="Nilesh@123" label={"Password"} />
+                    <div>
+                        <Button 
+                        onClick={async () => {
+                            const response = await axios.post("http://localhost:3000/api/v1/user/signup", {
+                                username,
+                                firstName,
+                                lastName,
+                                password
+                            });
+                            localStorage.setItem("token", response.data.token);
+                            navigate("/dashboard");  
+                        }} 
+                        label={"Sign up"} />
+                    </div>
+                    <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
                 </div>
-                <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
             </div>
         </div>
-    </div>
-}
-
+    );
+}; 

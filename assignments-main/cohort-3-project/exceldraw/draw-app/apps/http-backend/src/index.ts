6d90bb78d557990680acf14d63f3e8ec933138a6
Nilesh020910 <nileshsoni0209@gmail.com>
2025-02-08 21:55:34 +0530

    push

--- assignments-main/cohort-3-project/exceldraw/draw-app/apps/http-backend/src/index.ts
+++ assignments-main/cohort-3-project/exceldraw/draw-app/apps/http-backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "./config";
+import { middleware } from "./middleware";
 
 const app = express();
 
@@ -8,10 +10,12 @@ app.post("/signup", async (req, res) => {
 })
 
 app.post("/signin", async (req, res) => {
-    
+    const userId = 1;
+    const token = jwt.sign({ userId }, JWT_SECRET);
+    res.json({ token })
 })
 
-app.post("/room", async (req, res) => {
+app.post("/room", middleware, async (req, res) => {
     
 })
 

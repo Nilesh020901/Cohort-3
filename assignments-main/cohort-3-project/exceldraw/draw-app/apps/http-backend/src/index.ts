fb537ab99157235b9133e9be21cc438af4550036
Nilesh020910 <nileshsoni0209@gmail.com>
2025-02-10 10:25:00 +0530

    push

--- assignments-main/cohort-3-project/exceldraw/draw-app/apps/http-backend/src/index.ts
+++ assignments-main/cohort-3-project/exceldraw/draw-app/apps/http-backend/src/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import { CreateUserSchema, SigninSchema, RoomSchema } from "@repo/common/types";
@@ -11,36 +10,37 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
     
-    const valid = CreateUserSchema.safeParse(req.body);
-    if(!valid.success) {
+    const parsedData = CreateUserSchema.safeParse(req.body);
+    if(!parsedData.success) {
         res.status(404).json({ message: "Invalid Inputs" });
         return;
     }
-
-    const { username, password, name, photo} = valid.data;
-
     try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-
         const user = await prismaClient.user.create({
            data: {
-            username,
-            password: hashedPassword,
-            name,
-            photo: photo || ""
+            email: parsedData.data?.username,
+            password: parsedData.data?.password,
+            name: parsedData.data?.name,
            },
-        })
-    } catch (error) {
+        });
         
+        res.status(201).json({ 
+            userId: user.id,
+            message: "You are Signed-up"});
+    } catch (error) {
+        res.status(404).json({ message: "User already exists"});
     }
 })
 
 app.post("/signin", async (req, res) => {
-    const data = SigninSchema.safeParse(req.body);
-    if(!data) {
+    const parsedData = SigninSchema.safeParse(req.body);
+    if(!parsedData) {
         res.status(404).json({ message: "Invalid Inputs" });
         return;
     }
+
+    
+
     const userId = 1;
     const token = jwt.sign({ userId }, JWT_SECRET);
     res.json({ token })

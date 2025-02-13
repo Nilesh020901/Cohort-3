9cb1976dc6fbcaf2dfd5af5f0f975709b44bb850
Nilesh020910 <nileshsoni0209@gmail.com>
2025-02-13 23:04:46 +0530

    push

--- assignments-main/cohort-3-project/courses-app/index.js
+++ assignments-main/cohort-3-project/courses-app/index.js
@@ -1,25 +1,19 @@
 const express = require ("express");
+
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
 const app = express();
 app.use(express.json());
 
-app.post("/user/sigup", async (req, res) => {
-    
-})
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/admin", adminRouter);
 
-app.post("/user/signin", async (req, res) => {
-    
-})
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
 
-app.get("user/purchase", async (req, res) => {
-
-})
-
-app.post("/course/purchase", async (req, res) => {
-    
-})
-
-app.get("/courses", async (req, res) => {
-    
-})
-
-app.listen(3000)
+main();
---
+++ assignments-main/cohort-3-project/courses-app/middleware/usermiddleware.js
@@ -0,0 +1,2 @@
+const jwt = require("jsonwebtoken");
+const { JWT_USER_PASSWORD } = require("../config");

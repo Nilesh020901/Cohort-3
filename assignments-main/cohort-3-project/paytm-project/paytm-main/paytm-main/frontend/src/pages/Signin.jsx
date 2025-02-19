97f3032a87ccb684154305bb33b903e359b230d2
Nilesh020901 <nileshsoni0209@gmail.com>
2025-02-19 17:07:57 +0530

    push

--- assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/frontend/src/pages/Signin.jsx
+++ assignments-main/cohort-3-project/paytm-project/paytm-main/paytm-main/frontend/src/pages/Signin.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 import { BottomWarning } from "../components/BottomWarning";
 import { Button } from "../components/Button";
 import { Heading } from "../components/Heading";
@@ -11,23 +11,32 @@ export const Signin = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
-    return
-    <div className="bg-slate-300 h-screen flex justify-center">
-        <div className="flex flex-col justify-center">
-            <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-                <Heading label={"Sign in"} />
-                <SubHeading label={"Enter your credentials to access your account"} />
-                <InputBox onChange={(e) => {
-                    setUsername(e.target.value);
-                }} placeholder="nileshsoni0209@gmail.com" label={"Email"} />
-                <InputBox onChange={(e) => {
-                    setPassword(e.target.value);
-                }} placeholder="Nilesh@123" label={"Password"} />
-                <div className="pt-4">
-                    <Button label={"Sign in"} />
+    return (
+        <div className="bg-slate-300 h-screen flex justify-center">
+            <div className="flex flex-col justify-center">
+                <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+                    <Heading label={"Sign in"} />
+                    <SubHeading label={"Enter your credentials to access your account"} />
+                    <InputBox 
+                        onChange={(e) => {
+                            setUsername(e.target.value);
+                        }} 
+                        placeholder="nileshsoni0209@gmail.com" 
+                        label={"Email"} 
+                    />
+                    <InputBox 
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }} 
+                        placeholder="Nilesh@123" 
+                        label={"Password"} 
+                    />
+                    <div className="pt-4">
+                        <Button label={"Sign in"} />
+                    </div>
+                    <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
                 </div>
-                <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
             </div>
         </div>
-    </div>
+    );
 }

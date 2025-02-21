dacc274e41e28fa5f16c63bfb4543524ad355389
Nilesh020901 <nileshsoni0209@gmail.com>
2025-02-21 12:47:21 +0530

    push

---
+++ assignments-main/cohort-3-project/projects/draw-app/apps/http-backend/src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+
+export fnction middleware(req: Request, res: Response, next: NextFunction) {
+	try {
+		const token = req.headers["authorization"] ?? "";
+		const decoded = jwt.verify(token, JWT_SECRET);
+
+		if (decoded) {
+		// @ts-ignore: TODO: Fix this
+			req.userId = decoded.userId;
+			next();
+		} else {
+			res.status(403).json({ message: "Unauthorized" })
+		}
+	} catch(e) {
+		res.status(503).json({ message: "Internal Server Error" })
+	}
+}

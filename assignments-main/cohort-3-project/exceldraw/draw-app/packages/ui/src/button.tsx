ea8e58cd65cfe70b059f406fc9c6569e0d24de17
Nilesh020910 <nileshsoni0209@gmail.com>
2025-02-25 22:38:18 +0530

    push

--- assignments-main/cohort-3-project/exceldraw/draw-app/packages/ui/src/button.tsx
+++ assignments-main/cohort-3-project/exceldraw/draw-app/packages/ui/src/button.tsx
@@ -3,12 +3,14 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
+  variant: "primary" | "secondary";
   children: ReactNode;
   className?: string;
+  onClick?: () => void;
   appName: string;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, appName, variant, onClick  }: ButtonProps) => {
   return (
     <button
       className={className}

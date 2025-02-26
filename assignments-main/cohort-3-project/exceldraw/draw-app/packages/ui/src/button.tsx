0148c95b4ff913b1430ccbb3589fa824ea849b03
Nilesh020910 <nileshsoni0209@gmail.com>
2025-02-26 10:44:01 +0530

    try to buit signup and sign in button

--- assignments-main/cohort-3-project/exceldraw/draw-app/packages/ui/src/button.tsx
+++ assignments-main/cohort-3-project/exceldraw/draw-app/packages/ui/src/button.tsx
@@ -3,18 +3,27 @@
 import { ReactNode } from "react";
 
 interface ButtonProps {
-  variant: "primary" | "secondary";
-  children: ReactNode;
+  variant?: "primary" | "outline"; 
+  size?: "lg" | "sm";
   className?: string;
   onClick?: () => void;
-  appName: string;
+  children: ReactNode;
 }
 
-export const Button = ({ children, className, appName, variant, onClick  }: ButtonProps) => {
+export const Button = ({
+  variant = "primary",
+  size = "lg",
+  className = "",
+  onClick,
+  children,
+}: ButtonProps) => {
   return (
     <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      className={`flex items-center gap-2 font-fredoka rounded-2xl shadow-lg hover:shadow-xl transition-colors cursor-pointer
+        ${variant === "primary" ? "bg-indigo-600 text-white hover:bg-indigo-700" : "bg-white text-gray-700 border-2 border-gray-200 hover:border-gray-300"} 
+        ${size === "lg" ? "px-8 py-4 text-lg" : "px-4 py-2 text-sm"} 
+        ${className}`}
+      onClick={onClick}
     >
       {children}
     </button>

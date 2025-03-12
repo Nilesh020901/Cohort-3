3bd07f60708db10148037b7cf8511b74c1548678
Nilesh020910 <nileshsoni0209@gmail.com>
2025-03-12 10:11:06 +0530

    push

--- assignments-main/cohort-3-project/exceldraw/draw-app/apps/frontend/app/draw/index.ts
+++ assignments-main/cohort-3-project/exceldraw/draw-app/apps/frontend/app/draw/index.ts
@@ -1,7 +1,6 @@
 import { HTTP_BACKEND_URL } from "@/config";
 import axios from "axios";
-import { getExistingShapes } from "./http";
-import { clear } from "console";
+
 
 type Shape = {
     type: "rect",
@@ -86,5 +85,55 @@ export async function initDraw(canvas: HTMLCanvasElement, roomId: string, socket
         }))
     })
 
-    
+    canvas.addEventListener("mousemove", (e) => {
+        if(clicked) {
+            const width = e.clientX - startX;
+            const height = e.clientY - startY;
+            clearCanvas(existingShapes, canvas, ctx);
+            ctx.strokeStyle = "rgba(255, 255, 255)"
+            // @ts-ignore
+            const selectedTool = window.selectedTool;
+            if(selectedTool == "react") {
+                ctx.strokeRect(startX, startY, width, height);
+            } else if(selectedTool == "circle") {
+                const radius = Math.max(width, height) /2;
+                const centerX = startX + radius;
+                const centerY = startY + radius;
+                ctx.beginPath();
+                ctx.arc(centerX,centerY, radius, 0, 2 * Math.PI);
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+    })
+}
+
+function clearCanvas(existingShapes: Shape[], canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "rgba(255, 255, 255)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    existingShapes.map((shape) => {
+        if(shape.type == "rect") {
+            ctx.strokeStyle = "rgba(255, 255, 255)";
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        } else if(shape.type == "circle") {
+            ctx.beginPath();
+            ctx.arc(shape.centerX, shape.centerY, shape.radius, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.closePath();
+        }
+    })
+}
+
+async function getExistingShapes(roomId: string) {
+    const res = await axios.get(`${HTTP_BACKEND_URL}/shapes/${roomId}`);
+    const message = res.data.message;
+
+    const shapes: Shape[] = message.map((x: {message: string}) => {
+        const messageData = JSON.parse(x.message);
+        return messageData.shape;
+    })
+
+    return shapes;
 }

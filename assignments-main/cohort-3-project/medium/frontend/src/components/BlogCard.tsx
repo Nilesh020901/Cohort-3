5d9f6905981b58295cce251b13e99e157f228e79
Nilesh020910 <nileshsoni0209@gmail.com>
2025-03-24 21:02:53 +0530

    push

--- assignments-main/cohort-3-project/medium/frontend/src/components/BlogCard.tsx
+++ assignments-main/cohort-3-project/medium/frontend/src/components/BlogCard.tsx
@@ -1,3 +1,37 @@
 import { Link } from 'react-router-dom';
-import Avataar from 'avataar';
+import Avataar from 'Avataar';
 import ShareBtn from './ShareBtn';
+import Preview from './Preview';
+import EditBtn from './EditBtn';
+import DeleteBtn from './DeleteBtn';
+
+interface BlogCardProps {
+    authorName: string;
+    authorId: string;
+    title: string;
+    content: string;
+    id: string;
+    publisedDate: string;
+}
+
+export default function BlogCard({
+    authorName,
+    authorId,
+    title,
+    content,
+    id,
+    publisedDate,
+}: BlogCardProps) {
+    return (
+        <div className='pb-3 border-b border-slate-200'>
+            <div className='flex items-center justify-center gap-2 hover:scale-105'>
+                <Avataar name={authorName} />
+                <div className='text-base first-letter:uppercase'>
+                    {authorName}{" "}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+

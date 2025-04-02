e529bce40467188c7958e554389abc78d7d84eda
Nilesh020910 <nileshsoni0209@gmail.com>
2025-04-02 23:26:47 +0530

    push

--- assignments-main/cohort-3-project/medium/common/dist/index.d.ts
+++ assignments-main/cohort-3-project/medium/common/dist/index.d.ts
@@ -1,18 +1,17 @@
 import { z } from "zod";
 export declare const signupInput: z.ZodObject<{
-    username: z.ZodString;
     email: z.ZodString;
     password: z.ZodString;
+    name: z.ZodString;
 }, "strip", z.ZodTypeAny, {
-    username: string;
     email: string;
     password: string;
+    name: string;
 }, {
-    username: string;
     email: string;
     password: string;
+    name: string;
 }>;
-export type SignupInput = z.infer<typeof signupInput>;
 export declare const signinInput: z.ZodObject<{
     email: z.ZodString;
     password: z.ZodString;
@@ -23,29 +22,47 @@ export declare const signinInput: z.ZodObject<{
     email: string;
     password: string;
 }>;
-export type SigninInput = z.infer<typeof signinInput>;
-export declare const createPostInput: z.ZodObject<{
+export declare const updateUserDetailsInput: z.ZodObject<{
+    name: z.ZodOptional<z.ZodString>;
+    password: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    password?: string | undefined;
+    name?: string | undefined;
+}, {
+    password?: string | undefined;
+    name?: string | undefined;
+}>;
+export declare const blogCreateInput: z.ZodObject<{
     title: z.ZodString;
     content: z.ZodString;
+    published: z.ZodOptional<z.ZodString>;
 }, "strip", z.ZodTypeAny, {
     title: string;
     content: string;
+    published?: string | undefined;
 }, {
     title: string;
     content: string;
+    published?: string | undefined;
 }>;
-export type CreatePostInput = z.infer<typeof createPostInput>;
-export declare const updatePostInput: z.ZodObject<{
+export declare const blogUpdateInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
     id: z.ZodString;
-    title: z.ZodOptional<z.ZodString>;
-    content: z.ZodOptional<z.ZodString>;
+    published: z.ZodBoolean;
 }, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    published: boolean;
     id: string;
-    title?: string | undefined;
-    content?: string | undefined;
 }, {
+    title: string;
+    content: string;
+    published: boolean;
     id: string;
-    title?: string | undefined;
-    content?: string | undefined;
 }>;
-export type UpdatePostInput = z.infer<typeof updatePostInput>;
+export type signinInput = z.infer<typeof signinInput>;
+export type signupInput = z.infer<typeof signupInput>;
+export type updateUserDetailsInput = z.infer<typeof updateUserDetailsInput>;
+export type blogCreateInput = z.infer<typeof blogCreateInput>;
+export type blogUpdateInput = z.infer<typeof blogUpdateInput>;

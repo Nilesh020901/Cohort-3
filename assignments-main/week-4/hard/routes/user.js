16d316e6fab2e655840c94d3c4c93d63dabdf6fa
Nilesh020910 <nileshsoni0209@gmail.com>
2025-04-11 22:47:16 +0530

    push create signup sign -in

--- assignments-main/week-4/hard/routes/user.js
+++ assignments-main/week-4/hard/routes/user.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const router = Router();
-const userMiddleware = require("../middleware/user");
+const { userMiddleware, SECRET } = require("../middleware/user");
+const jwt = require("jsonwebtoken");
 
 // User Routes
 router.post('/signup',async (req, res) => {
@@ -11,23 +12,54 @@ router.post('/signup',async (req, res) => {
         if(user) {
             return res.status(404).json({ message: "You have already account" });
         }
+        
+        const newUser = new User({ username, password });
+        await newUser.save();
 
-        
+        const token = jwt.sign({ userId: newUser._id}, SECRET, { expiresIn: "1h" });
+        res.status(200).json({
+            message: "You are successfully",
+            token
+        })
     } catch (error) {
-        
+        res.status(500).json({
+            message: "Internal server Error"
+        })
     }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login',async (req, res) => {
      // Implement user login logic
+     const { username, password } = req.body;
+     try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({
+                message: "You not have account"
+            })
+        }
+
+        const token = jwt.sign({ userId: user._id}, SECRET, { expiresIn: "1h" });
+        res.status(200).json({
+            message: "You are logged-in",
+            token
+        });
+     } catch (error) {
+        res.status(500).json({
+            message: "Internal Server Error"
+        })
+     }
 });
 
 router.get('/todos', userMiddleware, (req, res) => {
     // Implement logic for getting todos for a user
+    
 });
 
 router.post('/logout', userMiddleware, (req, res) => {
     // Implement logout logic
+    // No server-side action needed for JWT logout
+    res.status(200).json({ message: "Logout successful" });
 });
 
 module.exports = router

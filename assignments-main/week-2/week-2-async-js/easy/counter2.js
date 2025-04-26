ab4cb71b7655f65d199ccf04a15eebaf8f2dbc95
Nilesh020910 <nileshsoni0209@gmail.com>
2025-04-26 23:04:10 +0530

    push learn mongoose connect and schema of mongoose

--- assignments-main/week-2/week-2-async-js/easy/counter2.js
+++
@@ -1,9 +0,0 @@
-let counter = 0;
-
-function updateCounter() {
-    counter++;
-    console.log(counter);
-}
-
-setTimeout(updateCounter, 1000); // This will only run once after 1 second
-setTimeout(updateCounter, 2000); // This will only run once after 2 seconds

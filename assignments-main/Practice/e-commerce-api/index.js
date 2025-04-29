245fac487d70ec3debfc018d0924b75fd8f3b930
Nilesh020910 <nileshsoni0209@gmail.com>
2025-04-30 00:08:26 +0530

    learn role and jwt

--- assignments-main/Practice/e-commerce-api/index.js
+++ assignments-main/Practice/e-commerce-api/index.js
@@ -3,11 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const authRouter = require("./routes/authRoutes");
 
 dotenv.config();
 
 app.use(cors);
 app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
 
 mongoose.connect(process.env.MONGO_URL, { userNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("MONGODB Connected"))
--- assignments-main/Practice/e-commerce-api/routes/authRoutes.js
+++ assignments-main/Practice/e-commerce-api/routes/authRoutes.js
@@ -1,7 +1,49 @@
 const express = require("express");
 const authRouter = express.Router();
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+authRouter.post("/signup", async (req, res) => {
+    try {
+        const { name, email, password, role } = req.body;
+
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(401).json({ message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword,
+            role,
+        });
+        await user.save();
+
+        const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        res.status(201).json({ message: "Signup successful", token })
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+authRouter.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: 'Invalid credentials' });
+
+        const isMatch = await bcrypt.compare( password, user.password );
+        if (!isMatch) return res.status(401).json({ message: "Invalid creadentials" });
+
+        const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        res.status(201).json({ message: "Sign-In successfully", token });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
 
 module.exports = {
     authRouter

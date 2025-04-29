ec163f1bddfdc38eb9b2b910f47bf708134b0c11
Nilesh020910 <nileshsoni0209@gmail.com>
2025-04-30 01:12:52 +0530

    push

--- assignments-main/Practice/e-commerce-api/index.js
+++ assignments-main/Practice/e-commerce-api/index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const authRouter = require("./routes/authRoutes");
+const productRouter = require("./routes/productRoutes");
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ app.use(cors);
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter);
+app.use("api/v1/product", productRouter);
 
 mongoose.connect(process.env.MONGO_URL, { userNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("MONGODB Connected"))

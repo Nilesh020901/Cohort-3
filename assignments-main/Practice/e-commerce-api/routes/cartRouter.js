e2b6fb565dab8aeaa7005240ad7f5253a472061a
Nilesh Soni <nileshsoni0209@gmail.com>
2025-04-30 19:57:56 +0530

    push not learn the things need to revise

--- assignments-main/Practice/e-commerce-api/routes/cartRouter.js
+++ assignments-main/Practice/e-commerce-api/routes/cartRouter.js
@@ -27,7 +27,7 @@ cartRouter.post("/add", authMiddleware, async (req, res) => {
                 cart.products[index].quantity += quantity;
             } else {
                 // Add new product
-                card.products.push({ product: productId, quantity });
+                cart.products.push({ product: productId, quantity });
             }
         }
 
@@ -37,6 +37,48 @@ cartRouter.post("/add", authMiddleware, async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+cartRouter.get("/", authMiddleware, async (req, res) => {
+    try {
+        const userId = req.user.userId;
+        const cart = await cart.findOne({ user: userId }).populate("products.product");
+
+        if (!cart) {
+            return res.status(404).json({ message: 'Cart not found' });
+        }
+        res.status(201).json({ cart });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+cartRouter.delete("/remove/:productId", async (req, res) => {
+    try {
+        const userId = req.user.userId
+        const productId = req.params.productId;
+
+        const cart = await cart.findOne({ user: userId });
+
+        if (!cart) {
+            return res.status(404).json({ message: 'Cart not found' });
+        }
+
+        const updateProducts = cart.products.filter(
+            item => item.product.toString() !== productId
+        );
+
+        if (updateProducts.length === cart.products.) {
+            return res.status(404).json({ message: 'Product not found in cart' });
+        }
+
+        cart.products = updateProducts;
+        await cart.save();
+
+        res.status(201).json({ message: 'Product removed from cart', cart });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
 
 module.exports = {
     cartRouter

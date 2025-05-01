e270555e0c1c5bcd70e1c7d1a03ad93bbdca8595
Nilesh020910 <nileshsoni0209@gmail.com>
2025-05-01 10:20:32 +0530

    push learn to create get methods

--- assignments-main/Practice/e-commerce-api/index.js
+++ assignments-main/Practice/e-commerce-api/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("api/v1/product", productRouter);
 app.use("api/v1/cart", cartRouter);
-app.use("api/v1/orders", orderRoutes)
+app.use("api/v1/orders", orderRouter);
 
 mongoose.connect(process.env.MONGO_URL, { userNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("MONGODB Connected"))

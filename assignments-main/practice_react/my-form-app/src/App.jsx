e2754ab80a9bae3e3d11d042fbe134d3e21a0b3f
Nilesh020910 <nileshsoni0209@gmail.com>
2025-05-06 10:07:57 +0530

    push the react learn function handle

---
+++ assignments-main/practice_react/my-form-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Form from "./Form";
+
+function App() {
+    return (
+        <div>
+            <h2 style={{ textAlign: "center" }}>Simple React Form</h2>
+            <Form />
+        </div>
+    );
+}
+
+export default App;

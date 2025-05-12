f356e1641728e418651a175237dc950dcaa09b2b
Nilesh020910 <nileshsoni0209@gmail.com>
2025-05-12 10:22:33 +0530

    push middleware and team module

---
+++ assignments-main/Practice/realtime-collab/src/models/Team.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const teamSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+    },
+    memebers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+},
+{
+    timestamps: true,
+});
+
+module.exports = mongoose.model("Team", teamSchema);

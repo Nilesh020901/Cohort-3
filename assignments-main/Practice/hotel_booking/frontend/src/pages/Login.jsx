e3d0c35969b459b0afee218b08585d2e6499b1e9
Nilesh Soni <nileshsoni0209@gmail.com>
2025-05-17 23:10:15 +0530

    push login and signup

--- assignments-main/Practice/hotel_booking/frontend/src/pages/Login.jsx
+++ assignments-main/Practice/hotel_booking/frontend/src/pages/Login.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+    const [ email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const { login } = useAuth();
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await login({ email, password })
+        } catch (error) {
+            setError('Invalid credentials');
+        }
+    }
     return (
         <div className="p-6 text-center">
             <h1 className="text-2xl font-bold">Login</h1>
+            {error && <p className="text-red-500">{error}</p>}
+            <form onSubmit={handleSubmit} className="mt-4 space-y-4">
+                <input 
+                    type="text"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full p-2 border rounded" 
+                />
+                <button type="submit" className="px-4 py-2 text-white bg-blue-500 rounded">
+                    Login
+                </button>
+            </form>
         </div>
     );
 }

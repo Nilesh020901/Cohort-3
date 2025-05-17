21bfc8bbea6ff2e7aaf75f76bdd1dbc798813d81
Nilesh020910 <nileshsoni0209@gmail.com>
2025-05-17 21:25:27 +0530

    Your commit message here

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+    return (
+        <div className="p-6 text-center">
+            <h1 className="text-2xl font-bold">Login</h1>
+        </div>
+    );
+}
+
+export default Login;

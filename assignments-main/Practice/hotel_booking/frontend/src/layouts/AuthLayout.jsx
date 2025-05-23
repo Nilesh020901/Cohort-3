59116275d881771af43db0f190d5611e08167fa9
Nilesh Soni <nileshsoni0209@gmail.com>
2025-05-23 07:45:00 +0000

    push done header

--- assignments-main/Practice/hotel_booking/frontend/src/layouts/AuthLayout.jsx
+++ assignments-main/Practice/hotel_booking/frontend/src/layouts/AuthLayout.jsx
@@ -1,8 +1,8 @@
-import { Children } from "react"
+import { children } from "react"
 
 const AuthLayout = () => {
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
-        {Children}
+        {children}
     </main>
 }
 

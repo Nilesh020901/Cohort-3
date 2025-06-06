fd4e5c04ef7e0c52d798be3f126b846f5a584811
Nilesh020910 <nileshsoni0209@gmail.com>
2025-06-06 10:21:08 +0530

    push landing page

--- assignments-main/Practice/hotel_booking/frontend/src/components/LovedHotels.jsx
+++ assignments-main/Practice/hotel_booking/frontend/src/components/LovedHotels.jsx
@@ -39,11 +39,43 @@ const hotels = [
 ];
 
 const LovedHotels = () => {
-    return (
-        <section className="py-6 px-4">
-            <h2 className="text-2xl font-semibold md-6">Hotels loved by guests</h2>
-        </section>
-    )
+  return (
+    <section className="py-6 px-4">
+      <h2 className="text-2xl font-semibold my-6">Hotels loved by guests</h2>
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+        {hotels.map((hotel, index) => (
+          <div
+            key={index}
+            className="bg-white rounded-2xl overflow-hidden shadow-lg hover:shadow-lg transition"
+          >
+            <div className="relative m-2 overflow-hidden rounded-2xl">
+              <img
+                src={hotel.image}
+                alt={hotel.name}
+                className="w-full h-48 object-cover"
+              />
+              <div className="absolute top-2 left-2 bg-green-100 text-green-800 text-xs font-semibold px-2 py-1 rounded-full">
+                {hotel.rating}
+              </div>
+              <div className="absolute top-2 right-2 bg-white/80 p-1 rounded-full">
+                <Heart className="w-4 h-4 text-gray-600" />
+              </div>
+            </div>
+            <div className="p-4">
+              <h3 className="text-base font-semibold">{hotel.name}</h3>
+              <p className="text-sm text-gray-500">{hotel.location}</p>
+              <div className="flex items-center justify-between mt-2">
+                <p className="text-sm font-bold text-green-800">
+                  from {hotel.price}
+                </p>
+                <ChevronRight className="w-4 h-4 text-gray-600" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default LovedHotels;
--- assignments-main/Practice/hotel_booking/frontend/srcc/layouts/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-import { children } from "react"
-
-const AuthLayout = () => {
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
-        {children}
-    </main>
-}
-
-export default AuthLayout;
